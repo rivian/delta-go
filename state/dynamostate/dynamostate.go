4a028e2e60de7f0c76d5dd0226756dd101d5ff68
Rahul Madnawat <rahulmadnawat@rivian.com>
2023-09-13 12:42:23 -0700

    Use defined mock client instead of old one

--- state/dynamostate/dynamostate.go
+++ state/dynamostate/dynamostate.go
@@ -13,56 +13,91 @@
 package dynamostate
 
 import (
+	"context"
 	"fmt"
 	"strconv"
 
-	"github.com/aws/aws-sdk-go/aws"
-	"github.com/aws/aws-sdk-go/service/dynamodb"
-	"github.com/aws/aws-sdk-go/service/dynamodb/dynamodbiface"
+	"github.com/aws/aws-sdk-go-v2/aws"
+	"github.com/aws/aws-sdk-go-v2/service/dynamodb"
+	"github.com/aws/aws-sdk-go-v2/service/dynamodb/types"
+	"github.com/rivian/delta-go/internal/dynamodbutils"
 	"github.com/rivian/delta-go/state"
 )
 
-const KEY string = "key"
-const VERSION string = "version"
+// Represents attribute names in DynamoDB items
+type AttributeName string
+
+const (
+	KeyAttr                            AttributeName = "key"
+	VersionAttr                        AttributeName = "version"
+	DefaultMaxRetryTableCreateAttempts uint16        = 20
+	DefaultRCU                         int64         = 5
+	DefaultWCU                         int64         = 5
+)
 
 type DynamoState struct {
 	Table  string
 	Key    string
-	Client dynamodbiface.DynamoDBAPI
+	Client dynamodbutils.DynamoDBClient
+}
+
+type Options struct {
+	MaxRetryTableCreateAttempts uint16
+	RCU                         int64
+	WCU                         int64
+}
+
+// Sets the default options
+func (opts *Options) setOptionsDefaults() {
+	if opts.MaxRetryTableCreateAttempts == 0 {
+		opts.MaxRetryTableCreateAttempts = DefaultMaxRetryTableCreateAttempts
+	}
+	if opts.RCU == 0 {
+		opts.RCU = DefaultRCU
+	}
+	if opts.WCU == 0 {
+		opts.WCU = DefaultWCU
+	}
 }
 
 // Compile time check that DynamoState implements state.StateStore
 var _ state.StateStore = (*DynamoState)(nil)
 
-func New(client dynamodbiface.DynamoDBAPI, tableName string, key string) (*DynamoState, error) {
+func New(client dynamodbutils.DynamoDBClient, tableName string, key string, opts Options) (*DynamoState, error) {
+	opts.setOptionsDefaults()
+
+	createTableInput := dynamodb.CreateTableInput{
+		KeySchema: []types.KeySchemaElement{
+			{
+				AttributeName: aws.String(string(KeyAttr)),
+				KeyType:       types.KeyTypeHash,
+			},
+		},
+		ProvisionedThroughput: &types.ProvisionedThroughput{
+			ReadCapacityUnits:  aws.Int64(opts.RCU),
+			WriteCapacityUnits: aws.Int64(opts.WCU),
+		},
+		TableName: aws.String(tableName),
+	}
+	dynamodbutils.TryEnsureDynamoDBTableExists(client, tableName, createTableInput, opts.MaxRetryTableCreateAttempts)
+
 	tb := new(DynamoState)
 	tb.Table = tableName
 	tb.Key = key
 	tb.Client = client
-	//TODO Check if table exists and handle exception
-	// describeTableInput := &dynamodb.DescribeTableInput{TableName: aws.String(tableName)}
-	// desciribeTableOutput, err := client.DescribeTable(describeTableInput)
-	// if desciribeTableOutput.Table.TableStatus != aws.String(dynamodb.TableStatusActive) {
-	// 	return nil, errors.New("dynamodb table is not active")
-	// }
-	// if err != nil {
-	// 	return nil, errors.New("dynamodb table is not active")
-	// }
 	return tb, nil
 }
 
 func (l *DynamoState) Get() (state.CommitState, error) {
 	input := &dynamodb.GetItemInput{
 		TableName: aws.String(l.Table),
-		Key: map[string]*dynamodb.AttributeValue{
-			KEY: {
-				S: aws.String(l.Key),
-			},
+		Key: map[string]types.AttributeValue{
+			string(KeyAttr): &types.AttributeValueMemberS{Value: l.Key},
 		},
 	}
 
 	// Call the GetItem operation to retrieve the item with the specified key value.
-	result, err := l.Client.GetItem(input)
+	result, err := l.Client.GetItem(context.TODO(), input)
 	if err != nil {
 		//TODO wrap error rather than printing
 		fmt.Println("Error retrieving item.", err)
@@ -74,7 +109,7 @@ func (l *DynamoState) Get() (state.CommitState, error) {
 		return state.CommitState{Version: -1}, err
 	}
 
-	versionValue := *result.Item["version"].S
+	versionValue := result.Item[string(VersionAttr)].(*types.AttributeValueMemberS).Value
 	version, err := strconv.Atoi(versionValue)
 	if err != nil {
 		//TODO wrap error rather than printing
@@ -92,18 +127,14 @@ func (l *DynamoState) Put(commitS state.CommitState) error {
 	// Create a PutItemInput object with the item data
 	input := &dynamodb.PutItemInput{
 		TableName: aws.String(l.Table),
-		Item: map[string]*dynamodb.AttributeValue{
-			KEY: {
-				S: aws.String(l.Key),
-			},
-			VERSION: {
-				S: aws.String(versionString),
-			},
+		Item: map[string]types.AttributeValue{
+			string(KeyAttr):     &types.AttributeValueMemberS{Value: l.Key},
+			string(VersionAttr): &types.AttributeValueMemberS{Value: versionString},
 		},
 	}
 
 	// Call PutItem to insert the new item into the table
-	_, err := l.Client.PutItem(input)
+	_, err := l.Client.PutItem(context.TODO(), input)
 	if err != nil {
 		//TODO wrap error rather than printing
 		fmt.Println("Error inserting item.", err)
