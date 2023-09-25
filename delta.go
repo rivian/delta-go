01632b26a1eb5ae116e34c4b79eac794cd6c0d29
Rahul Madnawat <rahulmadnawat@rivian.com>
2023-09-25 15:08:33 -0700

    Remove isolation level TODO comment

--- delta.go
+++ delta.go
@@ -1170,21 +1170,6 @@ func (transaction *Transaction) SetAppMetadata(appMetadata map[string]any) {
 // Commits the given actions to the Delta log.
 // This method will retry the transaction commit based on the value of `max_retry_commit_attempts` set in `TransactionOptions`.
 func (transaction *Transaction) Commit() (int64, error) {
-	// TODO: calculate isolation level to use when checking for conflicts.
-	// Leaving conflict checking unimplemented for now to get the "single writer" implementation off the ground.
-	// Leaving some commented code in place as a guidepost for the future.
-
-	// let no_data_changed = actions.iter().all(|a| match a {
-	//     Action::add(x) => !x.dataChange,
-	//     Action::remove(x) => !x.dataChange,
-	//     _ => false,
-	// });
-	// let isolation_level = if no_data_changed {
-	//     IsolationLevel::SnapshotIsolation
-	// } else {
-	//     IsolationLevel::Serializable
-	// };
-
 	PreparedCommit, err := transaction.PrepareCommit()
 	if err != nil {
 		log.Debugf("delta-go: PrepareCommit attempt failed. %v", err)
