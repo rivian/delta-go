e567b1c995acfb26ec2e12687ac5ffcf5c3f288b
Chelsea Jones <129552306+chelseajonesr@users.noreply.github.com>
2024-05-01 13:44:51 -0700

    Documentation updates (#229)
    * Update readme and comments
    
    * Update examples

--- examples/simple/main.go
+++ examples/simple/main.go
@@ -15,12 +15,14 @@ import (
 	"github.com/google/uuid"
 	"github.com/rivian/delta-go"
 	"github.com/rivian/delta-go/lock/filelock"
+	"github.com/rivian/delta-go/lock/nillock"
 	"github.com/rivian/delta-go/state/filestate"
 	"github.com/rivian/delta-go/storage"
 	"github.com/rivian/delta-go/storage/filestore"
 	log "github.com/sirupsen/logrus"
 )
 
+// To use pyspark locally to inspect the data after:
 // pyspark --packages io.delta:delta-core_2.12:2.4.0 --conf "spark.sql.extensions=io.delta.sql.DeltaSparkSessionExtension" --conf "spark.sql.catalog.spark_catalog=org.apache.spark.sql.delta.catalog.DeltaCatalog"
 func main() {
 	dir := "table"
@@ -30,7 +32,6 @@ func main() {
 	store := filestore.New(tmpPath)
 	state := filestate.New(tmpPath, "_delta_log/_commit.state")
 	lock := filelock.New(tmpPath, "_delta_log/_commit.lock", filelock.Options{})
-	checkpointLock := filelock.New(tmpPath, "_delta_log/_checkpoint.lock", filelock.Options{})
 	table := delta.NewTable(store, lock, state)
 	metadata := delta.NewTableMetaData("Test Table", "test description", new(delta.Format).Default(), getSchema(), []string{}, make(map[string]string))
 	err := table.Create(*metadata, new(delta.Protocol).Default(), delta.CommitInfo{}, []delta.Add{})
@@ -41,29 +42,25 @@ func main() {
 	fileName := fmt.Sprintf("part-%s.snappy.parquet", uuid.New().String())
 	filePath := filepath.Join(tmpPath.Raw, fileName)
 
-	//Make some data
+	// Generate some data
 	data := makeData(5)
-	stats := makeStats(data)
-	p, err := writeParquet(data, filePath)
+	_, err = writeParquet(data, filePath)
 	if err != nil {
 		log.Error(err)
 	}
 
-	add := delta.Add{
-		Path:             fileName,
-		Size:             p.Size,
-		DataChange:       true,
-		ModificationTime: time.Now().UnixMilli(),
-		Stats:            string(stats.JSON()),
-		PartitionValues:  make(map[string]string),
+	// Generate a commit that will add the data to the table
+	add, _, err := delta.NewAdd(store, storage.NewPath(fileName), make(map[string]string))
+	if err != nil {
+		log.Error(err)
 	}
-	transaction := table.CreateTransaction(delta.NewTransactionOptions())
 
+	// Save the commit.  When this succeeds, the data is now part of the table.
+	transaction := table.CreateTransaction(delta.NewTransactionOptions())
 	transaction.AddAction(add)
-	operation := delta.Write{Mode: delta.Overwrite}
+	operation := delta.Write{Mode: delta.Append}
 	appMetaData := make(map[string]any)
 	appMetaData["test"] = 123
-
 	transaction.SetAppMetadata(appMetaData)
 	transaction.SetOperation(operation)
 	v, err := transaction.Commit()
@@ -72,8 +69,15 @@ func main() {
 	}
 
 	log.Infof("version %d", v)
-	table.CreateCheckpoint(checkpointLock, delta.NewCheckpointConfiguration(), v)
 
+	// Write a checkpoint.
+	// If there is any possibility of multiple processes trying to write a checkpoint at the same version,
+	// you could use a lock here.
+	checkpointed, err := table.CreateCheckpoint(nillock.New(), delta.NewCheckpointConfiguration(), v)
+	if err != nil {
+		log.Error(err)
+	}
+	log.Infof("checkpoint created: %v", checkpointed)
 }
 
 type data struct {
@@ -86,8 +90,6 @@ type data struct {
 }
 
 func getSchema() delta.SchemaTypeStruct {
-
-	// schema := GetSchema(data)
 	schema := delta.SchemaTypeStruct{
 		Fields: []delta.SchemaField{
 			{Name: "id", Type: delta.Long, Nullable: false, Metadata: make(map[string]any)},
@@ -125,19 +127,6 @@ func makeData(n int) []data {
 	return d
 }
 
-func makeStats(data []data) delta.Stats {
-	stats := delta.Stats{}
-	for _, row := range data {
-		stats.NumRecords++
-		delta.UpdateStats(&stats, "id", &row.Id)
-		delta.UpdateStats(&stats, "t1", &row.Timestamp)
-		delta.UpdateStats(&stats, "label", &row.Label)
-		delta.UpdateStats(&stats, "value1", &row.Value1)
-		delta.UpdateStats(&stats, "value2", row.Value2)
-	}
-	return stats
-}
-
 type payload struct {
 	File *os.File
 	Size int64
@@ -158,8 +147,7 @@ func writeParquet[T any](data []T, filename string) (*payload, error) {
 	if err != nil {
 		return p, err
 	}
-	i, err := file.Write(buf.Bytes())
-	println(i)
+	_, err = file.Write(buf.Bytes())
 	if err != nil {
 		return p, err
 	}
